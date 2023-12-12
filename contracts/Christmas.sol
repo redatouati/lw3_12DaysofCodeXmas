01044970d8d3a69d2eeacc0e252b452ed9d80dfc
redatouati <32200840+redatouati@users.noreply.github.com>
2023-12-12 13:27:48 +0100

    deployed contract successfully

---
+++ contracts/Christmas.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Christmas {
+     /**
+     * @dev Prints Ho, Ho, Ho, Merry Christmas! string
+     */
+  function marrychristmas() public pure returns (string memory) {
+    return "Ho, Ho, Ho, Merry Christmas!";
+  }
+}
